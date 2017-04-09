bd7ccf7486aee389b6b2d5a1b5195cc6427a6f72
xli <xuenanli@xuenans-air.devices.brown.edu>
2017-04-09 13:12:23 -0400

    I change app.js file

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
           <h4>Welcome to React Again</h4>
+          <p>dhuksfhkdlugfslifjsdilfjdslivjndslivndslcnds\.\cndlcndslcsdlvjdcnddjsl</p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
