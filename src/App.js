080bca951cad3eaf0ae83d26d7dd3b5479587642
xli <xuenanli@xuenans-air.devices.brown.edu>
2017-04-09 13:15:25 -0400

    add

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
           <h2>Welcome to React</h2>
           <h4>Welcome to React Again</h4>
           <p>dhuksfhkdlugfslifjsdilfjdslivjndslivndslcnds\.\cndlcndslcsdlvjdcnddjsl</p>
+          <p>dhuksfhkdlugfslifjsdilfjdslivjndslivndslcnds\.\cndlcndslcsdlvjdcnddjsl</p>
+          <p>dhuksfhkdlugfslifjsdilfjdslivjndslivndslcnds\.\cndlcndslcsdlvjdcnddjsl</p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
